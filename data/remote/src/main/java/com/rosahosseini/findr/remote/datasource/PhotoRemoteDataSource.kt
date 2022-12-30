2a5f90d698212db7c22bb6b929b34705f21ff1a3
Rosa Hosseini <3427234-hosseini.simurg@users.noreply.gitlab.com>
2022-12-30 23:02:10 +0330

    Save api-key in buildConfig

--- data/remote/src/main/java/com/rosahosseini/findr/remote/datasource/PhotoRemoteDataSource.kt
+++ data/remote/src/main/java/com/rosahosseini/findr/remote/datasource/PhotoRemoteDataSource.kt
@@ -1,20 +1,22 @@
 package com.rosahosseini.findr.remote.datasource
 
+import com.rosahosseini.findr.remote.BuildConfig
 import com.rosahosseini.findr.remote.model.response.SearchResponseDto
-import com.rosahosseini.findr.remote.retrofit.API_KEY
 import com.rosahosseini.findr.remote.service.PhotoService
 import javax.inject.Inject
 
-class PhotoRemoteDataSource @Inject constructor(private val service: PhotoService) {
+class PhotoRemoteDataSource @Inject constructor(
+    private val service: PhotoService
+) {
 
     suspend fun search(text: String, limit: Int, page: Int): SearchResponseDto {
-        val response = service.search(API_KEY, text, page + 1, limit)
+        val response = service.search(BuildConfig.FLICKR_API_KEY, text, page + 1, limit)
         response.checkSuccess()
         return response
     }
 
     suspend fun getRecent(limit: Int, page: Int): SearchResponseDto {
-        val response = service.getRecent(API_KEY, page + 1, limit)
+        val response = service.getRecent(BuildConfig.FLICKR_API_KEY, page + 1, limit)
         response.checkSuccess()
         return response
     }
--- data/remote/src/main/java/com/rosahosseini/findr/remote/retrofit/Constants.kt
+++ data/remote/src/main/java/com/rosahosseini/findr/remote/retrofit/Constants.kt
@@ -1,4 +1,3 @@
 package com.rosahosseini.findr.remote.retrofit
 
-internal const val API_KEY = "1508443e49213ff84d566777dc211f2a"
 internal const val FLICKR_BASE_URL = "https://api.flickr.com/services/rest/"
