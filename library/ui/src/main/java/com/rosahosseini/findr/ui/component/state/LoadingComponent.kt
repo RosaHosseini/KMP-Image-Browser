e95fc21d63e25effd7b8fd90cc8f7b23852646f2
Rosa Hosseini <Rosa.Hosseini@emesa.nl>
2024-07-21 17:42:17 +0200

    Fix loading animation

--- library/ui/src/main/java/com/rosahosseini/findr/ui/component/state/LoadingComponent.kt
+++ library/ui/src/main/java/com/rosahosseini/findr/ui/component/state/LoadingComponent.kt
@@ -1,15 +1,12 @@
 package com.rosahosseini.findr.ui.component.state
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.rosahosseini.findr.ui.preview.ComponentPreviews
-import com.rosahosseini.findr.ui.theme.FindrTheme
 import com.rosahosseini.findr.ui.widget.DotsPulsing
 
 @Composable
